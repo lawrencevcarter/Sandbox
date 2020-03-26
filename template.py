5a73e77641064126413f2da951194cc0ef60b62d
Gantry York <gantry.york@gmail.com>
2020-03-25 20:00:32 -0700

    Adding template.py to repo

---
+++ template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# Import Standard Modules
+# -----------------------
+
+import os
+
+# Import Custom Modules
+# ---------------------
+
+
+def main():
+
+    print('Gantry is cool')
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
